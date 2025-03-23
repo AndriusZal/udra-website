9db20a91fa6c8f0a4a1162a9d4966cbfa1676d9a
Andrius Zaleckis <andrius.zaleckis@gmail.com>
2025-03-23 15:49:53 +0200

    Initial setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,157 @@
+"use client";
 import Image from "next/image";
 import styles from "./page.module.css";
+import { DM_Sans } from "next/font/google";
+import { useEffect, useRef, useState } from "react";
+
+interface Dimensions {
+    width: number;
+    height: number;
+}
+
+const dm_sans = DM_Sans({
+    weight: ["400", "700"],
+    style: "normal",
+});
+
+const getDynamicWidth = function ({ width }: Dimensions): number {
+    const firstDecrease = (x: number) => 0.7 + 0.3 * (1 - (x - 630) / 270);
+    const secondDecrease = (x: number) => firstDecrease(700) + (1 - firstDecrease(700)) * (1 - (x - 350) / 350);
+    if (width > 900) {
+        return width * 0.7 > 700 ? 700 : width * 0.7;
+    } else if (width <= 900 && width > 700) {
+        return width * firstDecrease(width);
+    } else {
+        return width * secondDecrease(width);
+    }
+};
+
+const getPath = function (width: number, height: number): string {
+    const componentHeight: number = height > 560 ? 550 : 500;
+    const componentWidth: number = width > 700 ? 700 : width < 350 ? 350 : width;
+    return `path("M 20 0 L ${componentWidth - 180} 0 A 20 20 0 0 1 ${componentWidth - 160} 20 L ${componentWidth - 160} 60 A 20 20 0 0 0 ${
+        componentWidth - 140
+    } 80 L ${componentWidth - 100} 80 A 20 20 0 0 1 ${componentWidth - 80} 100 L ${componentWidth - 80} ${
+        componentHeight - 100
+    } A 20 20 0 0 0 ${componentWidth - 60} ${componentHeight - 80} L ${componentWidth - 20} ${
+        componentHeight - 80
+    } A 20 20 0 0 1 ${componentWidth} ${componentHeight - 60} L ${componentWidth} ${componentHeight - 20} A 20 20 0 0 1 ${
+        componentWidth - 20
+    } ${componentHeight} L ${componentWidth - 60} ${componentHeight} A 20 20 0 0 1 ${componentWidth - 80} ${componentHeight - 20} L ${
+        componentWidth - 80
+    } ${componentHeight - 60} A 20 20 0 0 0 ${componentWidth - 100} ${componentHeight - 80} L 20 ${componentHeight - 80} A 20 20 0 0 1 0 ${
+        componentHeight - 100
+    } L 0 20 A 20 20 0 0 1 20 0 Z")`;
+};
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    const [dimensions, setDimensions] = useState<Dimensions>({ width: 0, height: 0 });
+    const dynamicTextBox = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+        const resizeObserver = new ResizeObserver((entries) => {
+            const { width, height } = entries[0].contentRect;
+            setDimensions({ width, height });
+        });
+        if (dynamicTextBox.current) {
+            resizeObserver.observe(dynamicTextBox.current);
+        }
+        return () => {
+            if (dynamicTextBox.current) {
+                resizeObserver.unobserve(dynamicTextBox.current);
+            }
+        };
+    }, []);
+    return (
+        <div className={styles.page}>
+            <header className={styles.header}>
+                <Image
+                    src="/logo.svg"
+                    alt="Udra logo"
+                    width={dimensions.width < 500 ? 200 : 250}
+                    height={dimensions.width < 500 ? 60 : 80}
+                    priority
+                />
+                {dimensions.width > 740 && (
+                    <div className={`${styles.socialMedia} ${dm_sans.className}`}>
+                        <p>Sekite mus: </p>
+                        <Image
+                            style={{ cursor: "pointer" }}
+                            onClick={() => open("https://www.instagram.com/udra_van/")}
+                            src="/instaLogo.svg"
+                            alt="Instagram link"
+                            width={42}
+                            height={41}
+                            priority
+                        />
+                        <Image
+                            style={{ cursor: "pointer" }}
+                            onClick={() => open("https://www.facebook.com/profile.php?id=61571981373934")}
+                            src="/fbLogo.svg"
+                            alt="Facebook Link"
+                            width={42}
+                            height={41}
+                            priority
+                        />
+                    </div>
+                )}
+            </header>
+            <main ref={dynamicTextBox} className={styles.main}>
+                <div
+                    className={styles.mainFrame}
+                    style={{
+                        clipPath: getPath(getDynamicWidth(dimensions), dimensions.height),
+                        height: dimensions.height > 560 ? 550 : 500,
+                        width: getDynamicWidth(dimensions) < 350 ? 350 : getDynamicWidth(dimensions),
+                    }}
+                >
+                    <div className={styles.tagline}>
+                        {dimensions.width > 400 && (
+                            <div className={styles.flag}>
+                                <div className={styles.yellow} />
+                                <div className={styles.green} />
+                                <div className={styles.red} />
+                            </div>
+                        )}
+                        <h1 className={styles.slogan}>NUOTYKIAI TIK PRASIDEDA</h1>
+                    </div>
+                    <div className={styles.subscribeContainer}>
+                        <p className={`${styles.description} ${dm_sans.className}`}>
+                            Naujas projektas kemperių entuziastams Lietuvoje. Prenumeruokite mūsų naujienlaiškį ir gaukite naujienas apie
+                            Udros startą pirmi!
+                        </p>
+                        <div className={styles.subscribe}>
+                            <input className={styles.subscribeInput} title="email" placeholder="Jūsų el. paštas"></input>
+                            <button className={`${styles.subscribeButton} ${dm_sans.className}`}>PRENUMERUOTI</button>
+                        </div>
+                    </div>
+                </div>
+            </main>
+            <footer className={`${styles.footer} ${dm_sans.className}`}>
+                {dimensions.width < 740 && (
+                    <div className={`${styles.socialMedia} ${dm_sans.className}`}>
+                        <p>Sekite mus: </p>
+                        <Image
+                            style={{ cursor: "pointer" }}
+                            onClick={() => open("https://www.instagram.com/udra_van/")}
+                            src="/instaLogo.svg"
+                            alt="Instagram link"
+                            width={42}
+                            height={41}
+                            priority
+                        />
+                        <Image
+                            style={{ cursor: "pointer" }}
+                            onClick={() => open("https://www.facebook.com/profile.php?id=61571981373934")}
+                            src="/fbLogo.svg"
+                            alt="Facebook Link"
+                            width={42}
+                            height={41}
+                            priority
+                        />
+                    </div>
+                )}
+                <p>© Udra Van, 2025</p>
+            </footer>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
